86a5d0b3b928c0768caa3d40ff7bc5674cb82c66
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-12-04 17:10:14 +0100

    AoC24: Saved some loops iteration starting from idx error

--- advent-of-code-2024/src/day02.rs
+++ advent-of-code-2024/src/day02.rs
@@ -56,18 +56,21 @@
 
 // Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
 
-fn is_safe<'a>(v: impl Iterator<Item = u32> + Clone) -> bool {
-    let mut iter = v.clone(); // clone only 2 elements
-    let first = iter.next().unwrap();
-    let second = iter.next().unwrap();
+fn is_safe<'a>(v: impl Iterator<Item = u32> + Clone) -> (bool, usize) {
+    // take first 5 elements to decide if the list is increasing or decreasing
+    let iter = v.clone().take(5);
+    let count_inc = iter.clone().zip(iter.clone().skip(1)).filter(|(a, b)| a < b).count();
+    let is_increasing = count_inc > 2;
 
-    let is_increasing = first < second;
-
-    v.clone().zip(v.skip(1)).all(|(a, b)| {
-        let diff = b.abs_diff(a);
-
-        ((is_increasing && a <= b) || (!is_increasing && a >= b)) && diff > 0 && diff <= 3
-    })
+    let mut idx = 0;
+    (
+        v.clone().enumerate().zip(v.skip(1)).all(|((index, a), b)| {
+            let diff = b.abs_diff(a);
+            idx = index;
+            ((is_increasing && a <= b) || (!is_increasing && a >= b)) && diff > 0 && diff <= 3
+        }),
+        idx, // return index of the error
+    )
 }
 
 #[must_use]
@@ -82,18 +85,21 @@ pub fn solve(input: &str) -> (i32, i32) {
             .filter_map(Result::ok)
             .collect::<Vec<u32>>();
 
-        if is_safe(nums.iter().copied()) { // zero cost copy those types
+        let (safe, idx_error) = is_safe(nums.iter().copied()); // zero cost copy those types
+
+        if safe {
             p1 += 1;
 
             // skip the check of dampener, is already safe
             continue;
         }
 
-        for i in 0..nums.len() {
+        for i in idx_error..nums.len() {
             let part1 = &nums[..i];
             let part2 = &nums[i + 1..];
 
-            if is_safe(part1.iter().chain(part2.iter()).copied()) {
+            let (safe, _) = is_safe(part1.iter().chain(part2.iter()).copied());
+            if safe {
                 p2 += 1;
 
                 // if we find a safe report, we can break the loop
@@ -111,12 +117,12 @@ mod tests {
 
     #[test]
     fn safety() {
-        assert_eq!(is_safe([7, 6, 4, 2, 1].iter().copied()), true);
-        assert_eq!(is_safe([1, 2, 7, 8, 9].iter().copied()), false);
-        assert_eq!(is_safe([9, 7, 6, 2, 1].iter().copied()), false);
-        assert_eq!(is_safe([1, 3, 2, 4, 5].iter().copied()), false);
-        assert_eq!(is_safe([8, 6, 4, 4, 1].iter().copied()), false);
-        assert_eq!(is_safe([1, 3, 6, 7, 9].iter().copied()), true);
+        assert_eq!(is_safe([7, 6, 4, 2, 1].iter().copied()), (true, 3));
+        assert_eq!(is_safe([1, 2, 7, 8, 9].iter().copied()), (false, 1));
+        assert_eq!(is_safe([9, 7, 6, 2, 1].iter().copied()), (false, 2));
+        assert_eq!(is_safe([1, 3, 2, 4, 5].iter().copied()), (false, 1));
+        assert_eq!(is_safe([8, 6, 4, 4, 1].iter().copied()), (false, 2));
+        assert_eq!(is_safe([27, 24, 26, 29, 30, 33, 36].iter().copied()), (false, 0));
     }
 
     #[test]
